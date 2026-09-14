f8b5084e85217a0f77fa672b25203dc81e99fbd6
hasib005-0 <hasibulislam0822@gmail.com>
2026-09-14 10:29:46 +0600

    vite yml package

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/Dev_Stack/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
